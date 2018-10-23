5a1db420816233b331e3ceea86ddd9741302230b
chronicSurfer <kyle.umbarger@gmail.com>
2018-10-23 16:38:03 -0700

    Quick Save

--- chickTech.js
+++ chickTech.js
@@ -1,9 +1,3 @@
-// var xhr = new XMLHttpRequest();
-// xhr.open("GET", "https://api.meetup.com/2/events?key=5c103fb263438792137465744f197b&group_urlname=girl-develop-it-orange-county&sign=true", true);
-// xhr.send(null);
-//
-// console.log(xhr);
-
 var settings = {
     "url": "https://api.meetup.com/2/events?key=5c103fb263438792137465744f197b&group_urlname=ChickTech-Orange-County&sign=true",
     "method": "GET",
