134802867558840c774bf3b7480b3945e029da5c
chronicSurfer <kyle.umbarger@gmail.com>
2018-10-23 15:46:53 -0700

    Chick Tech Request Complete

--- chickTech.js
+++ chickTech.js
@@ -5,17 +5,13 @@
 // console.log(xhr);
 
 var settings = {
-    "async": true,
-    "crossDomain": true,
     "url": "https://api.meetup.com/2/events?key=5c103fb263438792137465744f197b&group_urlname=ChickTech-Orange-County&sign=true",
-    "method": "GET"
-    // "headers": {
-    //     "cache-control": "no-cache",
-    //     "Postman-Token": "41f55fc5-ee41-4cc6-a4d5-b0d9c0211ba2",
-        // "Access-Control-Allow-Origin": "http://localhost:63342"
-
+    "method": "GET",
+    dataType: "jsonp",
+    success: function (response) {
+        console.log(response);
+    },
+    error: err=>console.log(err)
 }
 
-$.ajax(settings).done(function (response) {
-    console.log(response);
-});
+$.ajax(settings)
