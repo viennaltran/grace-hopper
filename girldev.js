63af3288fe456344fabe5aada4543ad1d8bb850e
viennaltran <lanvienhtran@gmail.com>
2018-10-23 18:36:39 -0700

    creating dynamically and adding to DOM

--- girldev.js
+++ girldev.js
@@ -9,7 +9,7 @@ function initializeApp(){
 
 var settings = {
     url: "http://api.meetup.com/2/events?key=6d367432270505e343b4d7c60634879&group_urlname=girl-develop-it-orange-county&sign=true",
-    method: "GET",
+    // method: "GET",
     dataType:"jsonp",
     success: function(response){
         console.log(response);
