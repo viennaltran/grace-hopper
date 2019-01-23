cef207507ac0c2deec893ace9bb46f8a842fcdda
Erin Tait <erinmtait@gmail.com>
2019-01-23 12:15:14 -0800

    page now scrolls to top when clicking on things in nav bar and back to events page

--- main.js
+++ main.js
@@ -162,6 +162,9 @@ function showEventsPage (){
     $('#gallery').addClass("hidePage");
     $("#event-chosen").addClass("hidePage").removeClass("event_chosen");
     $("#twitter-and-google-maps").addClass("hidePage").removeClass("twitter_and_google_maps");
+    window.setTimeout(function() {
+        $(window).scrollTop(0); 
+    }, 0);
 }
 
 function initMap () {
@@ -193,6 +196,9 @@ function showGallery(){
     $("#event-chosen").addClass("hidePage").removeClass("event_chosen");
     $("#twitter-and-google-maps").addClass("hidePage").removeClass("twitter_and_google_maps");
     $("#gallery").removeClass("hidePage");
+    window.setTimeout(function() {
+        $(window).scrollTop(0); 
+    }, 0);
 }
 
 // function addClickHandlerToSubmitButton(){
